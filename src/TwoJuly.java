66161123b7eaf69cbda47200367bf28e28cbb6e9
Vishesh-techno <sonivishesh099@gmail.com>
2025-07-02 11:57:09 +0530

    Basic Revision

--- src/TwoJuly.java
+++ src/TwoJuly.java
@@ -1,16 +1,24 @@
 import java.util.Scanner;
 
 public class TwoJuly {
+//    Write a program to print whether a number is even or odd, also take input from the user.
     public static void evenAndOdd(int n){
         if(n%2==0){
             System.out.println("The given no. is Even");
-        }
+        }else{
         System.out.println("The given no. is Odd");
+        }
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the no. ");
         int n = sc.nextInt();
         evenAndOdd(n);
+        sc.nextLine();
+//        Take name as input and print a greeting message for that particular name.
+        System.out.print("Enter the Name: ");
+        String str = sc.nextLine();
+        System.out.print("Welcome back " + str);
     }
 }
