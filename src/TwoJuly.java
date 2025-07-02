0c745805b92257c82e18733efd6ba3011d3d4897
Vishesh-techno <sonivishesh099@gmail.com>
2025-07-02 12:19:57 +0530

    Basic Revision

--- src/TwoJuly.java
+++ src/TwoJuly.java
@@ -1,3 +1,4 @@
+import java.util.Calendar;
 import java.util.Scanner;
 
 public class TwoJuly {
@@ -18,7 +19,34 @@ public class TwoJuly {
         int SI = (principle*rate*time)/100;
         return SI;
     }
-
+//    Take in two numbers and an operator (+, -, *, /) and calculate the value. (Use if conditions)
+    public static int calculator(int a, int b){
+        Scanner sc = new Scanner(System.in);
+        int result = 0;
+        System.out.println("Enter an operator (+, -, *, /): ");
+        char op = sc.next().charAt(0);
+        switch (op){
+            case'+':
+                result = a+b;
+                break;
+            case'-':
+                result = a-b;
+                break;
+            case'*':
+                result = a*b;
+                break;
+            case'/':
+                if(b!=0){
+                result = a/b;
+                }else{
+                    System.out.println("Cannot be divide by zero");
+                }
+                break;
+            default:
+                System.out.println("Invalid Operator: ");
+        }
+        return result;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the no. ");
@@ -36,5 +64,10 @@ public class TwoJuly {
         System.out.print("Enter the time: ");
         int T = sc.nextInt();
         System.out.println(simpleIntrest(P,R,T));
+        System.out.println("Enter the value of a: ");
+        int a = sc.nextInt();
+        System.out.println("Enter the value of b: ");
+        int b = sc.nextInt();
+        System.out.println("Result: " + calculator(a,b));
     }
 }
