511feb8c6ba5f54e6b4790150ac951b7f3470863
Vishesh-techno <sonivishesh099@gmail.com>
2025-07-05 13:12:02 +0530

    Solving String Question

--- src/FiveJuly.java
+++ src/FiveJuly.java
@@ -19,8 +19,24 @@ public class FiveJuly {
         }
         return str.toString();
     }
+
+    public static boolean arrayStringsAreEqual(String[] word1, String[] word2){
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+
+        for(String s1: word1){
+            sb1.append(s1);
+        }
+        for(String s2: word2){
+            sb2.append(s2);
+        }
+        return sb1.toString().equals(sb2.toString());
+    }
     public static void main(String[] args) {
         String s = "G()()al";
+        String[] s1 = {"ab", "c"};
+        String[] s2 = {"a", "bc"};
         System.out.println(goalInterpretation(s));
+        System.out.println(arrayStringsAreEqual(s1, s2));
     }
 }
