f1d825abb79de2ea8752e66456c4bfe250f768c8
Vishesh-techno <sonivishesh099@gmail.com>
2025-07-05 14:20:22 +0530

    Solving String Question

--- src/FiveJuly.java
+++ src/FiveJuly.java
@@ -1,36 +1,50 @@
 public class FiveJuly {
-//    Goal Parser Interpretation
-    public static String goalInterpretation(String s){
+    //    Goal Parser Interpretation
+    public static String goalInterpretation(String s) {
         StringBuilder str = new StringBuilder();
         int i = 0;
-        while(i<=s.length()){
-            if(s.charAt(i) == 'G'){
+        while (i <= s.length()) {
+            if (s.charAt(i) == 'G') {
                 str.append('G');
                 i++;
-            }else{
-                if (s.charAt(i+1) == ')' ){
+            } else {
+                if (s.charAt(i + 1) == ')') {
                     str.append('o');
-                    i+=2;
-                }else{
+                    i += 2;
+                } else {
                     str.append("al");
-                    i+=4;
+                    i += 4;
                 }
             }
         }
         return str.toString();
     }
 
-    public static boolean arrayStringsAreEqual(String[] word1, String[] word2){
+    public static boolean arrayStringsAreEqual(String[] word1, String[] word2) {
         StringBuilder sb1 = new StringBuilder();
         StringBuilder sb2 = new StringBuilder();
 
-        for(String s1: word1){
+        for (String s1 : word1) {
             sb1.append(s1);
         }
-        for(String s2: word2){
+        for (String s2 : word2) {
             sb2.append(s2);
         }
         return sb1.toString().equals(sb2.toString());
+    }
+
+    public static String toLowerCase(String s){
+//        for any lowercase value you need to add 32 because the alphabet in ASCII
+        StringBuilder result = new StringBuilder();
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch >='A' && ch<='Z'){
+                result.append((char)(ch+32));
+            }else{
+                result.append(ch);
+            }
+        }
+        return result.toString();
     }
     public static void main(String[] args) {
         String s = "G()()al";
@@ -38,5 +52,7 @@ public class FiveJuly {
         String[] s2 = {"a", "bc"};
         System.out.println(goalInterpretation(s));
         System.out.println(arrayStringsAreEqual(s1, s2));
+        System.out.println(s);
+        System.out.println(toLowerCase(s));
     }
 }
