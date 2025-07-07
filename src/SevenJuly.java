55ad8c8c374df1764224732b0a1f108cb6d433a7
Vishesh-techno <sonivishesh099@gmail.com>
2025-07-07 19:59:17 +0530

    Solving String Question

--- src/SevenJuly.java
+++ src/SevenJuly.java
@@ -17,10 +17,29 @@ public class SevenJuly {
         }
         return sb.toString();
     }
+    public static boolean jugdeCircle(String moves){
+        int n = 0;
+        int n1 = 0;
+        for(int i=0; i<moves.length(); i++){
+            char ch = moves.charAt(i);
+            if(ch=='U'){
+                n += 1;
+            }else if(ch=='D'){
+                n -= 1;
+            }else if(ch=='L'){
+                n1 -= 1;
+            }else{
+                n1 += 1;
+            }
+        }
+        return (n==0&&n1==0);
+    }
     public static void main(String[] args) {
         String[] sarr = {"a", "ab", "abc", "d"};
         System.out.println(numOfStrings(sarr, "abc"));
         String str = "Vishesh is Learning Java";
         System.out.println(reverseWords(str));
+        String str1 = "UDUDUDUDUDDLRLRRRLLRRRL";
+        System.out.println(jugdeCircle(str1));
     }
 }
