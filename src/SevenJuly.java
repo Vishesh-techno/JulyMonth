e9d4f312cd8b3175c3c7e2434c6f7619bda81e3a
Vishesh-techno <sonivishesh099@gmail.com>
2025-07-07 18:52:05 +0530

    Solving String Question

--- src/SevenJuly.java
+++ src/SevenJuly.java
@@ -1,15 +1,26 @@
 public class SevenJuly {
-    public static int numOfStrings(String[] patterns, String word){
+    public static int numOfStrings(String[] patterns, String word) {
         int count = 0;
-        for(String s : patterns){
-            if(word.contains(s)){
+        for (String s : patterns) {
+            if (word.contains(s)) {
                 count++;
             }
         }
         return count;
     }
+
+    public static String reverseWords(String s) {
+        String[] word = s.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for(String words : word){
+            sb.append(new StringBuilder(words).reverse().append(" "));
+        }
+        return sb.toString();
+    }
     public static void main(String[] args) {
-        String[] sarr = {"a", "ab", "abc","d"};
+        String[] sarr = {"a", "ab", "abc", "d"};
         System.out.println(numOfStrings(sarr, "abc"));
+        String str = "Vishesh is Learning Java";
+        System.out.println(reverseWords(str));
     }
 }
