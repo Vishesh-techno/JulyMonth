383300eada4676fa757e70a94ab105f2913fc9fd
Vishesh-techno <sonivishesh099@gmail.com>
2025-07-08 11:56:17 +0530

    Solving String Question

--- src/EightJuly.java
+++ src/EightJuly.java
@@ -34,6 +34,29 @@ public class EightJuly {
     public static int strStr(String haystack, String needle) {
         return haystack.indexOf(needle);
     }
+    public static boolean validPalindromeII(String s){
+        int start = 0;
+        int end = s.length()-1;
+        while(start<end){
+            if(s.charAt(start) == s.charAt(end)){
+                start++;
+                end--;
+            }else{
+                return isPalindrome(s, start+1, end) || isPalindrome(s , start , end-1);
+            }
+        }
+        return true;
+    }
+    private static boolean isPalindrome(String s, int start, int end){
+        while (start < end) {
+            if (s.charAt(start) != s.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
 
     public static void main(String[] args) {
         String input = "A man, a plan, a canal, Panama";
@@ -48,5 +71,12 @@ public class EightJuly {
             System.out.println("It's not a palindrome.");
         }
         System.out.println(strStr("sadbutsad", "sad"));
+        String str = "abbaceabba";
+        System.out.println(validPalindromeII(str));
+        if (validPalindromeII(str)) {
+            System.out.println("It's a palindrome.");
+        } else {
+            System.out.println("It's not a palindrome.");
+        }
     }
 }
