32fe75fe9f7f3452231aa979f6a3c600e8ba06fb
Vishesh-techno <sonivishesh099@gmail.com>
2025-07-08 11:09:21 +0530

    Solving String Question

--- src/EightJuly.java
+++ src/EightJuly.java
@@ -15,10 +15,31 @@ public class EightJuly {
 
         return true;
     }
+    public static boolean validPalindromeBetter(String s) {
+        String s1 = s.replaceAll("[\\s,]", "").toLowerCase();  // Remove spaces and commas
+//        char[] ch = s1.toCharArray();
+
+        int start = 0, end = s1.length() - 1;
+
+        while (start < end) {
+            if (s1.charAt(start) != s1.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
+    }
 
     public static void main(String[] args) {
         String input = "A man, a plan, a canal, Panama";
         if (validPalindrome(input)) {
+            System.out.println("It's a palindrome.");
+        } else {
+            System.out.println("It's not a palindrome.");
+        }
+        if (validPalindromeBetter(input)) {
             System.out.println("It's a palindrome.");
         } else {
             System.out.println("It's not a palindrome.");
