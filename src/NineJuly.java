9ab42aab16b184c17233e02a78a4347d3dc8d09b
Vishesh-techno <sonivishesh099@gmail.com>
2025-07-09 11:49:26 +0530

    Basic Revision

--- src/NineJuly.java
+++ src/NineJuly.java
@@ -17,11 +17,31 @@ public class NineJuly {
         }
         return count;
     }
+    public static boolean hasConsecutiveOnes(String s) {
+        char[] ch = s.toCharArray();
+        int count = 0;
+        if(ch[ch.length-1]=='1'){
+            count = 1;
+        }else{
+            count = 0;
+        }
+        for(int i=0; i<ch.length; i++){
+            if (ch[i] == '1'&&ch[i+1] != '1'){
+                count++;
+                if(count>1){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
     public static void main(String[] args) {
         String str = "ababaabababacab";
         String word = "ab";
         System.out.println(maxRepeating(str, word));
-        System.out.println(maxRepeating(str, word));
+        System.out.println(maxRepeatingOptimal(str, word));
+        System.out.println(hasConsecutiveOnes("10"));
+        System.out.println();
     }
 }
