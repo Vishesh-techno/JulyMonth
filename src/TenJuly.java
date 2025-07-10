28dbb4b8851adc86e150c83a7af3c5a7ced47d37
Vishesh-techno <sonivishesh099@gmail.com>
2025-07-10 14:24:50 +0530

    Basic Revision

--- src/TenJuly.java
+++ src/TenJuly.java
@@ -17,7 +17,22 @@ public class TenJuly {
         return new String(ch1);
     }
 
+    public static boolean isValid(String s) {
+        char[] ch = s.toCharArray();
+        int left = 0;
+        int right = ch.length-1;
+        while(left<right){
+            if((ch[left]=='{' && ch[right] != '}')||(ch[left]=='[' && ch[right] != ']')||(ch[left]=='(' && ch[right] != ')')){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
     public static void main(String[] args) {
         System.out.println(reversePrefix("abcdefd", 'd'));
+        System.out.println(isValid("{[()]}"));
+        System.out.println(isValid("{[()()]}"));
     }
 }
