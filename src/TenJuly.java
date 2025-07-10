f535ff67c0866df93bbbc048984e2533f1cf16dc
Vishesh-techno <sonivishesh099@gmail.com>
2025-07-10 14:50:01 +0530

    Basic Revision

--- src/TenJuly.java
+++ src/TenJuly.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class TenJuly {
     public static String reversePrefix(String word, char ch){
         int index = word.indexOf(ch);
@@ -30,9 +32,29 @@ public class TenJuly {
         }
         return true;
     }
+    public static int lengthOfLastWord(String s) {
+        String[] s1 = s.split("\\s");
+        int end = s1.length-1;
+        return s1[end].length();
+    }
+    public static int lengthOfLastWordOptimal(String s) {
+        int length = 0;
+        int i = s.length()-1;
+        while(i>=0&&s.charAt(i)==' '){
+            i--;
+        }
+        while (i>=0&&s.charAt(i)!=' '){
+            length++;
+            i--;
+        }
+        return length;
+    }
     public static void main(String[] args) {
         System.out.println(reversePrefix("abcdefd", 'd'));
         System.out.println(isValid("{[()]}"));
         System.out.println(isValid("{[()()]}"));
+        System.out.println(lengthOfLastWord("Hello World"));
+        System.out.println(lengthOfLastWordOptimal("Hello World"));
+        
     }
 }
