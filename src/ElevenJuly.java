a0e3233e7d4722d2ca8e2878b8417de628038e28
Vishesh-techno <sonivishesh099@gmail.com>
2025-07-11 15:03:03 +0530

    Basic Revision

---
+++ src/ElevenJuly.java
@@ -0,0 +1,47 @@
+public class ElevenJuly {
+    // Om namah Shivay
+    public static boolean canReach(String s, int minJump, int maxJump){
+        char[] ch = s.toCharArray();
+        int start = 0;
+        int n = ch.length;
+
+        while (start < n) {
+            if (start == n - 1) return true;
+
+            boolean jumped = false;
+            for (int j = start + minJump; j <= Math.min(start + maxJump, n - 1); j++) {
+                if (ch[j] == '0') {
+                    start = j;
+                    jumped = true;
+                    // break;
+                }
+            }
+
+            if (!jumped) return false;
+        }
+
+        return false;
+    }
+    public static boolean checkPalindromeFormation(String a, String b) {
+        for (int i = 0; i < a.length(); i++) {
+            if (isPalindrome(a.substring(0, i) + b.substring(i)) ||
+                    isPalindrome(b.substring(0, i) + a.substring(i))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean isPalindrome(String s) {
+        int l = 0, r = s.length() - 1;
+        while (l < r && s.charAt(l) == s.charAt(r)) {
+            l++;
+            r--;
+        }
+        return l >= r;
+    }
+    public static void main(String[] args) {
+        System.out.println(canReach("011010", 2, 3));
+
+    }
+}
