74903ed73ae1ffa161a91ec27e564b4309923123
Vishesh-techno <sonivishesh099@gmail.com>
2025-07-12 13:09:31 +0530

    Basic Revision

--- src/TwelveJuly.java
+++ src/TwelveJuly.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class TwelveJuly {
     public static boolean areSentenceSimilar(String sentence1, String sentence2) {
         String[] array1 = sentence1.split(" ");
@@ -17,9 +19,35 @@ public class TwelveJuly {
         return index1 > end1 || index2 > end2;
     }
 
+    public static void rotateArray(int[] nums, int k) {
+        revNum(nums, 0, nums.length-1);
+        revNum(nums, 0 , k-1);
+        revNum(nums, k , nums.length-1);
+    }
+    public static void revNum(int[] nums, int start , int end){
+        while(start<=end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+
     public static void main(String[] args) {
         String s1 = "My Name is Vishesh Soni";
         String s2 = "My Father Name is Mukesh Soni";
         System.out.println(areSentenceSimilar(s1, s2));
+        int[] nums = {1,2,3,4,5,6,7};
+        int k = 4;
+        rotateArray(nums , k);
+//        Arrays.toString(nums);
+        System.out.print("[");
+        for(int num : nums){
+            System.out.print(num + " ");
+        }
+        System.out.print("]");
+
     }
 }
