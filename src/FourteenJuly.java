c7ef7badb83570ea978fc3b1dde2ee88283ae2ce
Vishesh-techno <sonivishesh099@gmail.com>
2025-07-14 20:41:12 +0530

    Basic Revision

--- src/FourteenJuly.java
+++ src/FourteenJuly.java
@@ -1,5 +1,30 @@
-public class FourteenJuly {
-    public static void main(String[] args) {
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
+public class FourteenJuly {
+    public static int[] intersection(int[] nums1, int[] nums2){
+        Set<Integer> s1 = new HashSet<>();
+        Set<Integer> s2 = new HashSet<>();
+        for(int num: nums1){
+            s1.add(num);
+        }
+        for(int num: nums2){
+            s2.add(num);
+        }
+        int[] res = new int[Math.max(s1.size(), s2.size())];
+        int k = 0;
+        for(int num: s1){
+            if(s2.contains(num)){
+                res[k] = num;
+                k++;
+            }
+        }
+        return Arrays.copyOfRange(res, 0, k);
+    }
+    public static void main(String[] args) {
+        int[] nums1 = {4,2,4,5,4};
+        int[] nums2 = {4,5,4,5,4};
+        System.out.println(Arrays.toString(intersection(nums1, nums2)));
     }
 }
