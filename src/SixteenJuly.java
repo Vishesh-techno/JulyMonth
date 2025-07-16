07553d2114b89239acd719eb01de9e785166bb25
Vishesh-techno <sonivishesh099@gmail.com>
2025-07-16 12:14:44 +0530

    Basic Revision

--- src/SixteenJuly.java
+++ src/SixteenJuly.java
@@ -1,3 +1,4 @@
+import javax.naming.PartialResultException;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -28,11 +29,24 @@ public class SixteenJuly {
         return new int[]{-1,-1};
     }
 
+    public static int[] twoSumOptimal(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
 
+        for(int i=0; i<nums.length; i++){
+            int lookUpNumber = target - nums[i];
+
+            if(map.containsKey(lookUpNumber)){
+                return new int[]{i, map.get(lookUpNumber)};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
     public static void main(String[] args) {
         int[] nums = {3,2,4};
         int tar = 6;
         System.out.println(Arrays.toString(twoSum(nums, tar)));
         System.out.println(Arrays.toString(twoSumBetter(nums, tar)));
+        System.out.println(Arrays.toString(twoSumOptimal(nums, tar)));
     }
 }
