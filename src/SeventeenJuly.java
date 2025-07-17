87caa42d79ae725180375a856e156846a7269545
Vishesh-techno <sonivishesh099@gmail.com>
2025-07-17 14:16:34 +0530

    Basic Revision

--- src/SeventeenJuly.java
+++ src/SeventeenJuly.java
@@ -39,7 +39,28 @@ public class SeventeenJuly {
 
         return true;
     }
+    public static boolean isAnagramUsingMap(String s, String t) {
+        if (t.length() != s.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c: s.toCharArray()) {
+            map.put(c, map.getOrDefault(c,0)+1);
+        }
+        for (char c: t.toCharArray()) {
+            map.put(c, map.getOrDefault(c,0)+1);
+        }
+        for (char c: map.keySet()) {
+            if (map.get(c) != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
     public static void main(String[] args) {
         System.out.println(firstUniqueChar("aabbccd"));
+        System.out.println(firstUniqueChar("aabbccddeffgg"));
     }
 }
