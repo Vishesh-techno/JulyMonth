097ab0e0e4c32e7e84e0e49fbb83422de1ce58f7
Vishesh-techno <sonivishesh099@gmail.com>
2025-07-24 14:37:03 +0530

    Basic Revision

--- src/TwentyFourJuly.java
+++ src/TwentyFourJuly.java
@@ -53,29 +53,29 @@ public class TwentyFourJuly {
         System.out.println("null");
     }
 
-    public static ListNode addTwoListNumbers(ListNode l1, ListNode l2){
+    public static ListNode addTwoListNumbers(ListNode l1, ListNode l2) {
         ListNode result = new ListNode(0);
         ListNode ptr = result;
 
         int carry = 0;
-        while(l1 != null || l2 != null){
+        while (l1 != null || l2 != null) {
             int sum = 0 + carry;
-            if(l1 != null){
+            if (l1 != null) {
                 sum = sum + l1.data;
                 l1 = l1.next;
             }
 
-            if(l2 != null){
+            if (l2 != null) {
                 sum = sum + l2.data;
                 l2 = l2.next;
             }
 
-            carry = sum /10;
+            carry = sum / 10;
             sum = sum % 10;
             ptr.next = new ListNode(sum);
             ptr = ptr.next;
         }
-        if(carry == 1){
+        if (carry == 1) {
             ptr.next = new ListNode(1);
         }
         return result.next;
