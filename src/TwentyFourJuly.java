f6a9bbfd1b032d3f19bb64138e6c01016cf51876
Vishesh-techno <sonivishesh099@gmail.com>
2025-07-24 12:21:24 +0530

    Basic Revision

--- src/TwentyFourJuly.java
+++ src/TwentyFourJuly.java
@@ -1,53 +1,58 @@
 public class TwentyFourJuly {
-    public static class ListNode{
+    public static class ListNode {
         int data;
         ListNode next;
-        public ListNode(int data){
+
+        public ListNode(int data) {
             this.data = data;
             this.next = null;
         }
     }
+
     public static ListNode head;
-    public static ListNode swapNodes(ListNode head, int k){
-        ListNode temp= head;
+
+    public static void swapNodes(ListNode head, int k) {
+        ListNode temp = head;
         int sz = 0;
-        while(temp != null){
+        while (temp != null) {
             sz++;
             temp = temp.next;
         }
         ListNode first = head;
         ListNode second = head;
-        for(int i=1; i<k; i++){
+        for (int i = 1; i < k; i++) {
             first = first.next;
         }
-        for(int i=1; i<sz-k+1; i++){
+        for (int i = 1; i < sz - k + 1; i++) {
             second = second.next;
         }
         int tem = first.data;
         first.data = second.data;
         second.data = tem;
-        return head;
     }
-    public void add(int data){
+
+    public void add(int data) {
         ListNode newNode = new ListNode(data);
-        if(head == null){
+        if (head == null) {
             head = newNode;
             return;
         }
         ListNode temp = head;
-        while(temp.next != null){
+        while (temp.next != null) {
             temp = temp.next;
         }
         temp.next = newNode;
     }
-    public void print(){
+
+    public void print() {
         ListNode temp = head;
-        while(temp != null){
+        while (temp != null) {
             System.out.print(temp.data + "==>");
             temp = temp.next;
         }
         System.out.println("null");
     }
+
     public static void main(String[] args) {
         TwentyFourJuly ll = new TwentyFourJuly();
         ll.add(1);
@@ -58,7 +63,6 @@ public class TwentyFourJuly {
         ll.print();
         swapNodes(head, 2);
         ll.print();
-
 
 
     }
