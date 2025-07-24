b89aaee05e7082449beabefeb9408c43f1dbf9cb
Vishesh-techno <sonivishesh099@gmail.com>
2025-07-24 14:36:47 +0530

    Basic Revision

--- src/TwentyFourJuly.java
+++ src/TwentyFourJuly.java
@@ -53,17 +53,57 @@ public class TwentyFourJuly {
         System.out.println("null");
     }
 
+    public static ListNode addTwoListNumbers(ListNode l1, ListNode l2){
+        ListNode result = new ListNode(0);
+        ListNode ptr = result;
+
+        int carry = 0;
+        while(l1 != null || l2 != null){
+            int sum = 0 + carry;
+            if(l1 != null){
+                sum = sum + l1.data;
+                l1 = l1.next;
+            }
+
+            if(l2 != null){
+                sum = sum + l2.data;
+                l2 = l2.next;
+            }
+
+            carry = sum /10;
+            sum = sum % 10;
+            ptr.next = new ListNode(sum);
+            ptr = ptr.next;
+        }
+        if(carry == 1){
+            ptr.next = new ListNode(1);
+        }
+        return result.next;
+    }
+
     public static void main(String[] args) {
         TwentyFourJuly ll = new TwentyFourJuly();
+        TwentyFourJuly ll1 = new TwentyFourJuly();
         ll.add(1);
         ll.add(4);
         ll.add(3);
         ll.add(2);
         ll.add(5);
+        ll1.add(1);
+        ll1.add(4);
+        ll1.add(3);
+        ll1.add(2);
+        ll1.add(5);
         ll.print();
         swapNodes(head, 2);
         ll.print();
-
+        System.out.println("Sum of Two Lists:");
+        ListNode temp = addTwoListNumbers(ll.head, ll1.head);
+        while (temp != null) {
+            System.out.print(temp.data + "==>");
+            temp = temp.next;
+        }
+        System.out.println("null");
 
     }
 }
