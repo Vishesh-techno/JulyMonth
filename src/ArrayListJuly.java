1c4e471b7b4b626a75eea2ce2e91a1cd89d321dd
Vishesh-techno <sonivishesh099@gmail.com>
2025-07-26 19:49:13 +0530

    Basic Revisions

--- src/ArrayListJuly.java
+++ src/ArrayListJuly.java
@@ -69,10 +69,10 @@ public class ArrayListJuly {
         ArrayList<Integer> l2 = new ArrayList<>();
         ArrayList<Integer> l3 = new ArrayList<>();
 
-        for(int i=1; i<5; i++){
+        for (int i = 1; i < 5; i++) {
             l1.add(i);
-            l2.add(i*2);
-            l3.add(i*3);
+            l2.add(i * 2);
+            l3.add(i * 3);
         }
 
         System.out.println(l1);
@@ -83,9 +83,9 @@ public class ArrayListJuly {
         mainList.add(l2);
         mainList.add(l3);
 
-        for(int i=0; i<mainList.size(); i++){
+        for (int i = 0; i < mainList.size(); i++) {
             ArrayList<Integer> currList = mainList.get(i);
-            for(int j=0; j<currList.size(); j++){
+            for (int j = 0; j < currList.size(); j++) {
                 System.out.print(currList.get(j) + " ");
             }
             System.out.println();
