b946712dddc63b4c348a19906eab56affc39aa0d
Vishesh-techno <sonivishesh099@gmail.com>
2025-07-28 14:43:40 +0530

    Basic Revisions

--- src/ArrayListJuly2.java
+++ src/ArrayListJuly2.java
@@ -1,36 +1,38 @@
 import java.util.ArrayList;
 
 public class ArrayListJuly2 {
-    public static int storeWater(ArrayList<Integer> height){
+    public static int storeWater(ArrayList<Integer> height) {
         int maxWater = 0;
         int h = 0, w = 0;
-        for(int i =0; i<height.size(); i++){
-            for (int j = i+1; j <height.size(); j++) {
+        for (int i = 0; i < height.size(); i++) {
+            for (int j = i + 1; j < height.size(); j++) {
                 h = Math.min(height.get(i), height.get(j));
-                w = j-i;
+                w = j - i;
                 int currWater = h * w;
                 maxWater = Math.max(currWater, maxWater);
             }
         }
         return maxWater;
     }
-    public static int storeWaterOptimal(ArrayList<Integer> height){
+
+    public static int storeWaterOptimal(ArrayList<Integer> height) {
         int maxWater = 0;
         int h = 0, w = 0;
-        int left = 0, right = height.size()-1;
-        while(left<right){
-                h = Math.min(height.get(left), height.get(right));
-                w = right-left;
-                int currWater = h * w;
-                maxWater = Math.max(currWater, maxWater);
-                if(height.get(left) < height.get(right)){
-                    left++;
-                }else{
-                    right--;
-                }
+        int left = 0, right = height.size() - 1;
+        while (left < right) {
+            h = Math.min(height.get(left), height.get(right));
+            w = right - left;
+            int currWater = h * w;
+            maxWater = Math.max(currWater, maxWater);
+            if (height.get(left) < height.get(right)) {
+                left++;
+            } else {
+                right--;
+            }
         }
         return maxWater;
     }
+
     public static void main(String[] args) {
         ArrayList<Integer> height = new ArrayList<>();
         height.add(1);
