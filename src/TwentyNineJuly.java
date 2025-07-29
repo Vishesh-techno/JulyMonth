68ef5f5652f101b1e23adf5552a83a62e1925c06
Vishesh-techno <sonivishesh099@gmail.com>
2025-07-29 21:07:30 +0530

    Basic Revisions

--- src/TwentyNineJuly.java
+++ src/TwentyNineJuly.java
@@ -6,10 +6,10 @@ public class TwentyNineJuly {
         int n = nums.length;
         int[] ans = new int[n];
         int sm = 0;
-        for(int i=0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             sm = 0;
-            for(int j=0; j<n; j++){
-                if(nums[j]<nums[i]){
+            for (int j = 0; j < n; j++) {
+                if (nums[j] < nums[i]) {
                     sm++;
                 }
             }
@@ -17,22 +17,24 @@ public class TwentyNineJuly {
         }
         return ans;
     }
+
     public static int[] createTargetArray(int[] nums, int[] index) {
         ArrayList<Integer> list = new ArrayList<>();
-        for(int i=0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length; i++) {
             list.add(index[i], nums[i]);
         }
 
         int[] ans = new int[list.size()];
-        for(int i=0; i < list.size(); i++) {
+        for (int i = 0; i < list.size(); i++) {
             ans[i] = list.get(i);
         }
         return ans;
     }
+
     public static void main(String[] args) {
-        int[] nums = {0,1,2,3,4};
-        int[] num = {8,1,2,2,3};
-        int[] index = {0,1,2,2,1};
+        int[] nums = {0, 1, 2, 3, 4};
+        int[] num = {8, 1, 2, 2, 3};
+        int[] index = {0, 1, 2, 2, 1};
         System.out.println(Arrays.toString(createTargetArray(nums, index)));
         System.out.println(Arrays.toString(smallerNumbersThanCurrent(num)));
     }
