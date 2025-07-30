83b457cac396d04fec934529306b91319fa42025
Vishesh-techno <sonivishesh099@gmail.com>
2025-07-30 19:43:53 +0530

    Basic Revisions

--- src/ThirtyJuly.java
+++ src/ThirtyJuly.java
@@ -32,6 +32,16 @@ public class ThirtyJuly {
         }
         return ms;
     }
+
+    public int[] buildArray(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+        for(int i=0; i<n; i++){
+            ans[i] = nums[nums[i]];
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
 
     }
