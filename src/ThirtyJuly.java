32dc2ce10bfc5493e29be948fe22edf18dfdd849
Vishesh-techno <sonivishesh099@gmail.com>
2025-07-30 19:47:20 +0530

    Basic Revisions

--- src/ThirtyJuly.java
+++ src/ThirtyJuly.java
@@ -35,7 +35,7 @@ public class ThirtyJuly {
         return ms;
     }
 
-    public static  int[] buildArray(int[] nums) {
+    public static int[] buildArray(int[] nums) {
         int n = nums.length;
         int[] ans = new int[n];
         for (int i = 0; i < n; i++) {
@@ -45,8 +45,8 @@ public class ThirtyJuly {
     }
 
     public static void main(String[] args) {
-        int[] nums = {0,2,1,5,3,4};
-        int[] num = {-2,1,-3,4,-1,2,1,-5,4};
+        int[] nums = {0, 2, 1, 5, 3, 4};
+        int[] num = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
 
         System.out.println(Arrays.toString(plusOne(nums)));
         System.out.println(Arrays.toString(buildArray(nums)));
