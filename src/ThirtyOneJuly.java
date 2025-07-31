9a9761735952ca78c36c63191024201048b35991
Vishesh-techno <sonivishesh099@gmail.com>
2025-07-31 20:39:15 +0530

    Basic Revisions

--- src/ThirtyOneJuly.java
+++ src/ThirtyOneJuly.java
@@ -14,7 +14,27 @@ public class ThirtyOneJuly {
         }
         return new int[] { -1, -1 };
     }
+    public static boolean isPerfectSquare(int num) {
+        long start = 2;
+        long end = num/2;
+        if (num < 2) {
+            return true;
+        }
+        while (start <= end) {
+            long mid = start + (end-start)/2;
+            long square = mid*mid;
+            if(square == num){
+                return true;
+            }else if (square<num){
+                start = mid+1;
+            }else{
+                end = mid-1;
+            }
+        }
+        return false;
+    }
     public static void main(String[] args) {
         int[] nums = {2,7,11,15};
+        int target = 9;
     }
 }
